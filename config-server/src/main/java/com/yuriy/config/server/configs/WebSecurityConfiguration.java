1a274267192bc3fd82e046e42eb354a8a0bd9978
Iurii Iakymchuk <lurii.lakymchuk@usoftware.co>
2022-07-14 17:36:36 +0300

    Implement config server

---
+++ config-server/src/main/java/com/yuriy/config/server/configs/WebSecurityConfiguration.java
@@ -0,0 +1,22 @@
+package com.yuriy.config.server.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+// This configuration is needed for disabling security when it's needed to encrypt sensitive information using /encrypt URL
+//@Configuration
+//@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .anyRequest().authenticated().and()
+                .httpBasic();
+        ;
+    }
+
+
+}
